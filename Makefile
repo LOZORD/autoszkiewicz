907976c00eba35d53690151d2a9f4dde7166147f
LOZORD <ljrudberg@gmail.com>
2014-09-26 21:16:01 -0500

    Started Makefile and better testing

---
+++ Makefile
@@ -0,0 +1,3 @@
+clean: ; rm *.class *.bmp *.jpg *.tiff
+
+test1:  ; javac Test1.java; java Test1; open test_img.bmp
--- Test1.java
+++ Test1.java
@@ -1,27 +1,28 @@
 class Test1
 {
-	public static void main (String [] args)
-	{
-		int dim = 64;
-		
-		AutoImg myImg = new AutoImg(dim, dim);
-		
-		int red = 0xff0000;
-		
-		int green = 0x00ff00;
-		
-		//first draw a diagonal line
-		for(int a = 0; a < dim; a++)
-		{
-			myImg.set(a,a, red);
-		}
-		
-		//then draw a square
-		
-		myImg.drawRectOutline(0, 0, 16, 16, green);
-		
-		myImg.write();
-		
-		System.out.println("Written!");
-	}
+  public static void main (String [] args)
+  {
+    System.out.println("A green square with a red diagonal line");
+
+    int dim = 64;
+
+    AutoImg myImg = new AutoImg(dim, dim);
+
+    int red = 0xff0000;
+
+    int green = 0x00ff00;
+
+    //first draw a diagonal line
+    for(int a = 0; a < dim; a++)
+    {
+      myImg.set(a,a, red);
+    }
+
+    //then draw a square
+    myImg.drawRectOutline(0, 0, 16, 16, green);
+
+    myImg.write();
+
+    System.out.println("Written!");
+  }
 }
